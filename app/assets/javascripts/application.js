8e0d897ed7fbc7bc5f68ea486c7503dbf0a56239
John Elder <johne4196@gmail.com>
2016-10-11 12:56:03 +0000

    tweaked js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require turbolinks
 //= require jquery
-//= require jquery_ujs
+// require jquery_ujs
 //= require tinymce-jquery
-// require turbolinks
 //= require bootstrap-sprockets
 //= require_tree .
